7806f37b34cd9b0b13e2ee47eafef7841189ca45
nelson <n4345370@gmail.com>
2026-07-29 11:09:57 +0200

    xml con atributos

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    try {
+
+        Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new File("datos.xml"));
+
+       System.out.println("Elemento raiz:" + document.getDocumentElement().getNodeName());
+
+        NodeList listaEmpleados = document.getElementsByTagName("empleado");
+        for (int i = 0; i < listaEmpleados.getLength(); i++) {
+            Node nodo = listaEmpleados.item(i);
+            System.out.println("Elemento:" + nodo.getNodeName() + " " + (i + 1));
+
+            //nos aseguramos que lo que leemos es un nodo y no un comentario por ejemplo
+            if (nodo.getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) nodo;
+                System.out.println("id: " + element.getAttribute("id"));
+                System.out.println("Nombre: " + element.getElementsByTagName("nombre").item(0).getTextContent());
+                System.out.println("username: " + element.getElementsByTagName("username").item(0).getTextContent());
+                System.out.println("password: " + element.getElementsByTagName("password").item(0).getTextContent());
+            }
         }
-        int variable = 0;
+
+
+    } catch (Exception e) {
+
+    }
+
 
     }
 }
